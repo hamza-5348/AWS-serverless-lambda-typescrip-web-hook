39877988d1096b0e52f327fcba8812730dd7ed2b
Hamza Hassan <hamza.hassan.5348@gmail.com>
2021-10-11 10:20:44 +0500

    testing pipeline

--- app/controller/mailgun.ts
+++ app/controller/mailgun.ts
@@ -51,7 +51,7 @@ export class MailgunController {
 
             const isPublished: Boolean = await this.publisher.publish(eventDto);
             const isSaved: Boolean = await this.store.set(eventRawData);
-            return MessageUtil.success({ isVerified, isPublished });
+            return MessageUtil.success({ isVerified, isPublished, isSaved });
 
         } catch (error) {
             return MessageUtil.error(406, error.message);
